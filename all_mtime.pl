6521ed75a5ce89f57cb75dead33b30aecb826d72
Dannen Harris <jher23@gmail.com>
2014-01-01 15:38:52 -0600

    initial add

---
+++ all_mtime.pl
@@ -0,0 +1,58 @@
+#!/usr/bin/perl -w
+use strict;
+use POSIX;
+
+my %seen;
+while (<*>){
+    next unless -f;
+    if ($_ =~ /(3g2|3gp|asf|avi|bmp|divx|flv|gif|jpg|jpeg|m1v|mov|mp4|mpeg|mpe|mpg|png|ram|rm|viv|wmv)$/i ) {
+    # get modify date from file
+    my $mdate = strftime "%Y_%m_%d_%H_%M_%S", localtime((lstat($_))[9]);
+
+    # set $was = filename
+    my $was = $_;
+
+    # lowercase
+    $_ =~ tr/A-Z/a-z/;
+
+    # brackets removal
+    $_ =~ s/(\)|\()//g;
+    $_ =~ s/(\]|\[)//g;
+
+    # remove whitespace
+    $_ =~ s/\s+//g;
+
+    # remove leading _
+    $_ =~ s/^_//;
+
+    # remove commas
+    $_ =~ s/,//g;
+
+    # remove plus
+    $_ =~ s/\+//g;
+
+    # remove amdpersand
+    $_ =~ s/\&//g;
+
+    # separator
+    $_ = "_" . $_;
+
+    # cull file extention from filename
+    #my $ext = ($_ =~ m/([^.]+)$/)[0];
+
+    # search and replace original filename with mdate and original filename
+    s/$_/$mdate$_/;
+
+    # already renamed
+    #next if $was =~ $mdate && print "hit\t$was\t$mdate\n";
+    next if $was =~ $mdate;
+
+    # push previous filename into array keyed on current filename
+    $seen{$_} = $was;
+    }
+}
+
+for my $fn (sort keys %seen){
+#    print "$fn\n";
+     rename $seen{$fn}, $fn;
+}
